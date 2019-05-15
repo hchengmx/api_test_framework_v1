05f81d97700f885a206da9756fcee0d34b1703b1
Hubert <hcheng_matrix@outlook.com>
2019-05-15 11:23:42 +0800

    3rd commit

--- run_all.py
+++ run_all.py
@@ -2,6 +2,7 @@ import unittest
 from HTMLTestReportCN import HTMLTestRunner
 import os
 
+
 # print(os.path.dirname(os.path.abspath(__file__)))
 # print(os.path.abspath(os.path.dirname(__file__)))
 # print(os.path.abspath(os.path.dirname(os.getcwd())))
@@ -13,3 +14,4 @@ suite = unittest.defaultTestLoader.discover(path, pattern='test_*.py')
 f = open("report.html", 'wb')  # 二进制写格式打开要生成的报告文件
 HTMLTestRunner(stream=f, title="Api Test", description="测试描述", tester="卡卡").run(suite)
 f.close()
+ 
